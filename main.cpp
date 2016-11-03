0875f0e0921d5bd3f0e0eeebc44fb60191fd0329
SiGe11 <sim.gergely@gmail.com>
2016-11-03 18:54:19 +0100

    English correction.

--- main.cpp
+++ main.cpp
@@ -17,17 +17,17 @@ int main()
     char oOperator;
     long double secondNumber;
     long double result;
-    std::string begenning;
+    std::string beginning;
 
     std::cout<<"DorcaLC v0.1"<<std::endl<<"The simple, open source calculator."<<std::endl<<"Type 'help' to see the usage."<<std::endl<<std::endl;
-    std::cin>>begenning;
-    if (begenning=="help")
+    std::cin>>beginning;
+    if (beginning=="help")
     {
     std::cout<<"Please give the first number, then press enter; the operator, then press enter; the second number; then press enter."<<std::endl<<"Then you will see the result."<<std::endl;
     firstNumber=usrInput();
     }
     else{
-    firstNumber = atoi(begenning.c_str());
+    firstNumber = atoi(beginning.c_str());
     }
 
 
